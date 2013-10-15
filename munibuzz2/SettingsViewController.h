846c8c806fa6258f49486d10f1ec0aa22088a645
ikiwi <kalai.wei@gmail.com>
2013-10-14 18:00:15 -0700

    adding a blank setting page

---
+++ munibuzz2/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  munibuzz2
+//
+//  Created by Kalai Wei on 10/14/13.
+//  Copyright (c) 2013 Kalai Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
